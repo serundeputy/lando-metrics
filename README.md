39e4e3605c6a7c205570cdc8cd82c39324a387ac
Geoff St. Pierre <serundeputy@gmail.com>
2021-04-03 13:36:20 -0400

    Adding in total num commands and growth.

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Start the app:
 lando start
 ```
 
-Currently there are hard coded lists for `months` and `providers` you wish to get the metrics for in the `app.py` file. So, if you want to adjust, add, or remove time periods and/or providers that you are interested in you need to edit those arrays.
+Currently there are hard coded lists for `months` and `providers` you wish to get the metrics for in the `app.py` file. So, if you want to adjust, add, or remove time periods and/or providers that you are interested in you need to edit those lists.
 
 To run the app:
 
--- app.py
+++ app.py
@@ -21,6 +21,23 @@ def month_and_year(month):
     return f'{pieces[1]} {pieces[2]}'
 
 
+def get_metric_count(data, group=[]):
+    if len(group) != 0:
+        metric = data.groupby(group).count()
+    else:
+        metric = data
+
+    return len(metric)
+
+
+def compute_growth(num_this_period, num_last_period):
+    return str(
+                round(
+                    ((num_this_period - num_last_period) / num_last_period) * 100
+                )
+            ) + '%'
+
+
 def get_recipe_metrics(start, end, recipe='lagoon'):
     ''' Get recipe metrics between start time and end time. '''
     start = int(datetime.strptime(start, '%d %B %Y').timestamp())
@@ -113,42 +130,40 @@ def get_recipe_metrics(start, end, recipe='lagoon'):
 
 
 months = [
-    ['01 July 2020', '31 July 2020'],
-    ['01 August 2020', '31 August 2020'],
-    ['01 September 2020', '30 September 2020'],
-    ['01 October 2020', '31 October 2020'],
-    ['01 November 2020', '30 November 2020'],
-    ['01 December 2020', '31 December 2020'],
-    ['01 January 2021', '31 January 2021'],
+    # ['01 July 2020', '31 July 2020'],
+    # ['01 August 2020', '31 August 2020'],
+    # ['01 September 2020', '30 September 2020'],
+    # ['01 October 2020', '31 October 2020'],
+    # ['01 November 2020', '30 November 2020'],
+    # ['01 December 2020', '31 December 2020'],
+    # ['01 January 2021', '31 January 2021'],
     ['01 February 2021', '28 February 2021'],
     ['01 March 2021', '31 March 2021'],
 ]
 providers = ['Pantheon', 'Localdev', 'PlatformSh', 'Lagoon']
 
-
-def get_uniq_count(data):
-    uniq_apps = data.groupby(['_source.app']).count()
-    return len(uniq_apps)
-
-
 for provider in providers:
-    last_month = 0
-    percent_growth = 0
+    last_month_uniq = 0
+    percent_growth_uniq = 0
+    last_month_commands = 0
+    percent_growth_commands = 0
     print(f'{provider}:')
-    print('\t\t\tMonth\t\t Unique Apps\t\tGrowth')
+    print('\t\tMonth\t\t Unique Apps\t\tGrowth\t\tNum Commands\t\tGrowth')
     for month in months:
         metric = get_recipe_metrics(month[0], month[1], provider.lower())
-        uniq_apps = get_uniq_count(metric)
-        if last_month > 0:
-            percent_growth = str(
-                    round(
-                        ((uniq_apps - last_month) / last_month) * 100
-                    )
-                ) + '%'
+        commands = get_metric_count(metric)
+        uniq_apps = get_metric_count(metric, ['_source.app'])
+        if last_month_uniq > 0:
+            uniq_app_pg = compute_growth(uniq_apps, last_month_uniq)
+            commands_pg = compute_growth(commands, last_month_commands)
         else:
-            percent_growth = 'NA'
+            uniq_app_pg = 'NA'
+            commands_pg = 'NA'
 
-        last_month = uniq_apps
+        last_month_uniq = uniq_apps
+        last_month_commands = commands
         print(
-            f'\t\t\t{month_and_year(month[0])}\t\t{uniq_apps}\t\t {percent_growth}'
+            f'\t\t{month_and_year(month[0])}'
+            + f'\t\t{uniq_apps}\t\t{uniq_app_pg}'
+            + f'\t\t{commands}\t\t\t{commands_pg}'
         )
